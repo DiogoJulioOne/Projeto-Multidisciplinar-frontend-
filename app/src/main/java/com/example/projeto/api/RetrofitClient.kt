45186f27bf5cf54d18df654d04e156ed7e32d91c
AndersonLS <indiedev.core@gmail.com>
2026-09-14 20:27:47 -0300

    Url update

--- app/src/main/java/com/example/projeto/api/RetrofitClient.kt
+++ app/src/main/java/com/example/projeto/api/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.0.75:5207/"
+    private const val BASE_URL = "http://10.0.0.2:5207/"
 
     private lateinit var tokenManager: TokenManager
 
