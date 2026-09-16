a150837358d14b3b18f1e322df8c0ee534bdbbda
DiogoJulioOne <dioguinho_julio@hotmail.com>
2026-09-16 20:42:57 -0300

    chat

--- app/src/main/java/com/example/projeto/MainActivity.kt
+++ app/src/main/java/com/example/projeto/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.projeto
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
@@ -11,8 +12,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.projeto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -29,35 +28,57 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
         }
+
         setSupportActionBar(binding.toolbar)
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+            supportFragmentManager.findFragmentById(
+                R.id.nav_host_fragment_content_main
+            ) as NavHostFragment
+
         val navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+
+        // BOTÃO DE MENSAGEM
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.MessagesFragment)
+        }
+
+        // Controla quando o botão aparece
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            if (destination.id == R.id.FirstFragment ||
+                destination.id == R.id.SecondFragment) {
+
+                binding.fab.hide()
+
+            } else {
+
+                binding.fab.show()
+            }
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -65,7 +86,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController =
+            findNavController(R.id.nav_host_fragment_content_main)
+
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
---
+++ app/src/main/java/com/example/projeto/MessagesFragment.kt
@@ -0,0 +1,39 @@
+package com.example.projeto
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.projeto.databinding.FragmentMessagesBinding
+
+class MessagesFragment : Fragment() {
+
+    private var _binding: FragmentMessagesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentMessagesBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
